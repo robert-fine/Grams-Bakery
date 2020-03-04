09e9b38547d37f1b4171150878f2607503bdf61a
Robert Fine <rob.fine09@gmail.com>
2020-03-03 23:23:22 -0600

    filter and search functions completed

---
+++ app.js
@@ -0,0 +1,39 @@
+let storeBtns = document.querySelectorAll(".store-btn");
+const storeSearch = document.querySelector(".store-search");
+let storeCardNames = document.querySelectorAll(".card-name");
+let storeCards = document.querySelectorAll(".card");
+
+storeBtns.forEach(btn =>
+  btn.addEventListener("click", function filterItems(e) {
+    e.preventDefault();
+
+    // change active button
+    const currentSelected = document.querySelector(".selected");
+    currentSelected.classList.remove("selected");
+    this.classList.add("selected");
+
+    // filter cards
+    storeCards.forEach(card => {
+      if (this.dataset.filter === "all") {
+        card.classList.remove("hidden");
+      } else {
+        card.classList.add("hidden");
+        if (card.dataset.item === this.dataset.filter) {
+          card.classList.remove("hidden");
+        }
+      }
+    });
+  })
+);
+
+// search filter
+storeSearch.addEventListener("keyup", () =>
+  storeCards.forEach(card => {
+    card.classList.add("hidden");
+    if (
+      card.innerHTML.toLowerCase().includes(storeSearch.value.toLowerCase())
+    ) {
+      card.classList.remove("hidden");
+    }
+  })
+);
